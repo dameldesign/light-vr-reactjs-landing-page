74b956fa526d268b057630ca8b7fe390fa07755a
dameldesign <98592632+dameldesign@users.noreply.github.com>
2022-10-04 20:55:29 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+// import components
+import Banner from './components/Banner';
+import Header from './components/Header';
+import NavMobile from './components/NavMobile';
+import Experience from './components/Experience';
+// import Video from './components/Video';
+import Headsets from './components/Headsets';
+import Testimonial from './components/Testimonial';
+import Explore from './components/Explore';
+
+
+
 
 const App = () => {
-  return <div>App</div>;
+  const [navMobile, setNavMobile] = useState(false);
+
+
+
+  return (
+    <div className='relative overflow-hidden before:w-[600px] before:h-[200px] before:bg-circle before:bg-no-repeat before:absolute before:-top-16 before:left-[600px] before:hidden before:lg:flex'>
+      <Header setNavMobile={setNavMobile} />
+      <Banner />
+      {/* mobile nav */}
+      <div
+        className={`${
+          navMobile ? 'right-0' : '-right-full'
+        } fixed top-0 bottom-0 w-48 transition-all`}
+      >
+        <NavMobile setNavMobile={setNavMobile} />
+      </div>
+      <Experience />
+      {/* <Video /> */}
+      <Headsets />
+      <Testimonial />
+      <Explore />
+    </div>
+  );
 };
 
 export default App;
